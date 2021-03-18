40b4465ec1508cd677e71afb6b27cbf78933fad9
Viacheslav Pliusnin <via.plsn@gmail.com>
2021-03-18 12:07:19 +0200

    generateBlocksInterval function added.

--- src/script.js
+++ src/script.js
@@ -12,8 +12,24 @@ function generateBlocks() {
         element.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
     });
 };
-generateBlocks()
 
-
-
-
+function generateBlocksInterval() {
+    const body = document.querySelector('body');
+    body.insertAdjacentHTML("afterbegin", '<div class="container">');
+    const container = document.getElementsByClassName('container');
+    for(let i = 0; i < 25; i++) {
+        container[0].insertAdjacentHTML("afterbegin", '<div class="square">');
+    }
+    const square = document.querySelectorAll('.square');
+    container[0].style.cssText = "display: flex; flex-wrap: wrap; height: 250px; width: 250px";
+    square.forEach(element => {
+        element.style.cssText = "display: inline-block; height: 50px; width: 50px";
+        element.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
+    });
+    setInterval(function() {
+        square.forEach(element => {
+            element.style.backgroundColor = '#' + (Math.random().toString(16) + '000000').substring(2,8).toUpperCase();
+        });
+    }, 1000);
+};
+generateBlocksInterval();
